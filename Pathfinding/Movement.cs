7e4ad83fb4ab9e9508ddd98ee02363853d20592d
NeosDragon <tranjames99@gmail.com>
2021-12-06 00:25:43 -0500

    Create Movement.cs

---
+++ Pathfinding/Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            //Sends Ray on where the user right clicks
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //Moves the player object where the user right clicks
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
